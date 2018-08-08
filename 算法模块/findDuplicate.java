2c88800cb13f31bb6df7cad4908bd53136e701e7
zhuguozhu123 <zgz1211651004>
2018-08-09 00:18:00 +0800

    algotithm01

---
+++ 算法模块/findDuplicate.java
@@ -0,0 +1,32 @@
+package p23;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 寻找重复数
+ * @author Guozhu Zhu
+ * @date 2018/8/9
+ * @version 1.0
+ *
+ */
+public class Test04 {
+	
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 3, 4, 5};
+		int ans = findDuplicate(arr);
+		System.out.println(ans);
+	}
+	
+	public static int findDuplicate(int[] arr) {
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < arr.length; i++) {
+			if (set.contains(arr[i])) {
+				return arr[i];
+			}
+			set.add(arr[i]);
+		}
+		return -1;
+	}
+
+}
