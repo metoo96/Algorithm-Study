74263d851ae482d750ce379467d8cc3022d41021
Warrior <15819064843@163.com>
2018-08-09 00:34:26 +0800

    Update search.java

--- 算法模块/search.java
+++ 算法模块/search.java
@@ -10,7 +10,7 @@ package p23;
 public class Test05 {
 	
 	public static void main(String[] args) {
-		int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	    int ans = search(arr, 5);
 	    System.out.println(ans);
 	}
