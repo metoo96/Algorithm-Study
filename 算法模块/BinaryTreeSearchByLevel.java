a89b15579cf80ff039b745c3a9df0396b68b7365
Warrior <15819064843@163.com>
2018-08-10 17:04:42 +0800

    Update BinaryTreeSearchByLevel.java

--- 算法模块/BinaryTreeSearchByLevel.java
+++ 算法模块/BinaryTreeSearchByLevel.java
@@ -1,18 +1,23 @@
 import java.util.*;
 
-
 /*
 public class TreeNode {
+
     int val = 0;
     TreeNode left = null;
     TreeNode right = null;
 
     public TreeNode(int val) {
         this.val = val;
-
     }
 
 }
+*/
+/*
+* 二叉树分层输出
+* @author Guozhu Zhu
+* @date 2018/8/10
+* @version 1.0
 */
 public class Solution {
     ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
