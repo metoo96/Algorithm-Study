0bb09cde07181518b20a77d9340e0f0dfceebad9
zhuguozhu123 <zgz1211651004>
2018-08-18 22:55:13 +0800

    zhuguozhu

---
+++ 算法模块/FindKthToTail.java
@@ -0,0 +1,32 @@
+/**
+* 链表中倒数第k个节点
+* @author Guozhu Zhu
+* @date 2018/8/18
+* @version 1.0
+*
+*/
+public class Solution {
+    
+    public ListNode FindKthToTail(ListNode head, int k) {
+        if (head == null || k <= 0) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        //先走k步
+        int i = 0;
+        while (i < k-1) {
+            if (fast.next == null) {
+                return null;
+            }
+            fast = fast.next;
+            i++;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+    
+}
