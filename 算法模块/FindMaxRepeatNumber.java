8e6a579d3b1df35764a15d6c6d59abbd9f103264
zhuguozhu123 <zgz1211651004>
2018-09-15 13:41:29 +0800

    zhuguozhu

---
+++ 算法模块/FindMaxRepeatNumber.java
@@ -0,0 +1,57 @@
+package p39;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+/**
+ * 前k个高频元素
+ * @author Guozhu Zhu
+ * @date 2018/9/15
+ * @version 1.0
+ *
+ */
+public class Test04 {
+	
+	/* ========== Test ========== */
+	public static void main(String[] args) {
+		int[] arr = {1, 1, 1, 2, 2, 3, 3, 3};
+		List<Integer> list = solution(arr, 2);
+		for (int i : list) {
+			System.out.println(i);
+		}
+	}
+	
+	public static ArrayList<Integer> solution(int[] nums, int k){
+        ArrayList<Integer> resList = new ArrayList<Integer>();
+		//大顶堆
+		PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>(new Comparator<Map.Entry<Integer, Integer>>(){
+			@Override
+			public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+				// TODO Auto-generated method stub
+				return o2.getValue()-o1.getValue();
+			}
+		});
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			if (!map.containsKey(nums[i])) {
+				map.put(nums[i], 1);
+			} else {
+				map.put(nums[i], map.get(nums[i])+1);
+			}
+		}
+		Set<Map.Entry<Integer, Integer>> set = map.entrySet();
+		for (Map.Entry<Integer, Integer> j : set) {
+			queue.offer(j);
+		}
+		for (int i = 0; i < k; i++) {
+			resList.add(queue.poll().getKey());
+		}
+		return resList;
+	}
+
+}
