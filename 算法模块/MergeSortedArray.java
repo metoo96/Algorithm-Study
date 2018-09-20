20011bac5d07d410784306a8860f361fece822dc
Warrior <15819064843@163.com>
2018-09-20 23:12:33 +0800

    Update MergeSortedArray.java

--- 算法模块/MergeSortedArray.java
+++ 算法模块/MergeSortedArray.java
@@ -1,3 +1,10 @@
+/**
+* 合并两个有序数组（其实就是用到了归并排序中的归并步骤的部分代码)
+* @author Guozhu Zhu
+* @date 2018/9/20
+* @version 1.0
+*
+*/
 class Solution {
     
     public void merge(int[] nums1, int m, int[] nums2, int n) {
