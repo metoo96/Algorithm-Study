2a43783e382ed4dee23530518356fd1463585989
zhuguozhu123 <zgz1211651004>
2018-09-20 23:07:25 +0800

    zhuguozhu

---
+++ 算法模块/MergeSortedArray.java
@@ -0,0 +1,26 @@
+class Solution {
+    
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] temp = new int[m+n];
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                temp[k++] = nums1[i++];
+            } else {
+                temp[k++] = nums2[j++];
+            }
+        }
+        while (i < m) {
+            temp[k++] = nums1[i++];
+        }
+        while (j < n) {
+            temp[k++] = nums2[j++];
+        }
+        for (int  a = 0; a < k; a++) {
+            nums1[a] = temp[a];
+        }
+    }
+    
+}
