8a2ce4f645b06684c54cca44adb7dd518f88cc7f
zhuguozhu123 <zgz1211651004>
2018-09-22 15:23:38 +0800

    zhuguozhu

---
+++ 算法模块/NumIslands.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+              if (grid[i][j] == '1') {
+                  IslandsCore(i, j, grid);
+                  count++;
+              }
+            }
+        }
+        return count;
+    }
+    
+    public void IslandsCore(int i, int j, char[][] grid) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1') {
+            return ;
+        }
+        grid[i][j] = 2;
+        IslandsCore(i+1,j, grid);
+        IslandsCore(i-1, j, grid);
+        IslandsCore(i, j-1, grid);
+        IslandsCore(i, j+1, grid);
+        
+    }
+    
+}
