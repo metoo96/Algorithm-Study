b30a7014d7f0baefa6fb24811eb153d455969120
zhuguozhu123 <zgz1211651004>
2018-09-22 15:04:22 +0800

    zhuguozhu

---
+++ 算法模块/BinaryTreeIsSymmetric.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+    
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return (left.val == right.val) && (isSymmetric(left.left, right.right)) && (isSymmetric(left.right, right.left));
+    }
+}
