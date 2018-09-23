4b5f583f8566e486e5439e23cb56a2b805a745a6
zhuguozhu123 <zgz1211651004>
2018-09-23 15:41:32 +0800

    zhuguozhu

---
+++ 算法模块/InterSection.java
@@ -0,0 +1,46 @@
+class Solution {
+    
+    public int[] intersection(int[] nums1, int[] nums2) {
+        ArrayList<Integer> resList = new ArrayList<Integer>();
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    resList.add(nums1[i]);
+                }
+            }
+        }  
+        Integer[] resArr = (Integer[])resList.toArray(new Integer[0]);
+        int[] arr = new int[resArr.length];
+        for (int i = 0; i < resArr.length; i++) {
+            arr[i] = resArr[i];
+        
+        }
+        //排序
+        Arrays.sort(arr);
+        int len = solution(arr);
+        int[] arr1 = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr1[i] = arr[i];
+        }
+        return arr1;
+    }
+    
+    //删除排序数组的重复项
+    public int solution(int[] array) {
+     if (array == null || array.length == 0) {
+            return 0;
+        } else if (array.length == 1) {
+            return 1;
+        } else {
+            int len = 0;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i-1] != array[i]) {
+                    len++;
+                    array[len] = array[i];
+                }
+            }
+            return len+1;
+        }
+    }
+    
+}
---
+++ 算法模块/moveZeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    
+    public void moveZeroes(int[] nums) {
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = 0; j < nums.length-1-i; j++) {
+                if (nums[j] == 0) {
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+    }
+
+}
