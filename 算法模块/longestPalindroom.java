e740ec69cdc3da84397ac9efcfa2d1a30b3dea35
Warrior <15819064843@163.com>
2018-09-23 23:50:18 +0800

    Update longestPalindroom.java

--- 算法模块/longestPalindroom.java
+++ 算法模块/longestPalindroom.java
@@ -1,32 +1,41 @@
+/**
+* 最长回文子串（dp实现)
+* @author Guozhu Zhu
+* @date 2018/9/23
+* @version 1.0
+*
+*/
 class Solution {
+    
     public String longestPalindrome(String s) {
-        //  长度为1，返回当前串
-    if (s.length()==1){
+    //长度为1，返回当前串
+    if (s.length() == 1) {
         return s;
     }
     //长度为2并且两个字符相等则返回
-    if (s.length()==2&&s.charAt(0)==s.charAt(1)){
+    if (s.length() == 2 && s.charAt(0) == s.charAt(1)) {
         return s;
     }
-        int n = s.length();
-        boolean[][] mark = new boolean[n][n];
-        int maxLen = 0;
-        int beginIndex = 0;
-        int lastIndex = 0;
-        for (int i = 0; i < n; i++) {
-            int j = i;
-            while (j >= 0) {
-                if ((s.charAt(i) == s.charAt(j)) && (i-j < 2 || mark[j+1][i-1])) {
-                    mark[j][i] = true;
-                     if (maxLen < i-j+1) {
+    int n = s.length();
+    boolean[][] mark = new boolean[n][n];
+    int maxLen = 0;
+    int beginIndex = 0;
+    int lastIndex = 0;
+    for (int i = 0; i < n; i++) {
+        int j = i;
+        while (j >= 0) {
+            if ((s.charAt(i) == s.charAt(j)) && (i-j < 2 || mark[j+1][i-1])) {
+                mark[j][i] = true;
+                if (maxLen < i-j+1) {
                     beginIndex = j;
                     lastIndex = i+1;
                     maxLen = i-j+1;
-                    }
                 }
-                j--;
             }
+            j--;
         }
-        return s.substring(beginIndex, lastIndex);
     }
+    return s.substring(beginIndex, lastIndex);
+    }
+    
 }
