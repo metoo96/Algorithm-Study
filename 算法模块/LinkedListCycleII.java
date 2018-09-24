957de277dec5c896fd22632beebb503a7f841db8
zhuguozhu123 <zgz1211651004>
2018-09-24 15:16:05 +0800

    zhuguozhu

---
+++ 算法模块/LinkedListCycleII.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode p1 = head;
+        ListNode p2 = head;
+        while (p2 != null && p2.next != null) {
+            p2 = p2.next.next;
+            p1 = p1.next;
+            if (p1 == p2) {
+                p2 = head;
+                while (p1 != p2){
+                    p1 = p1.next;
+                    p2 = p2.next;
+                }
+                return p1;
+            }
+        }
+        return null;
+    }
+}
