c74e38135a958911ad0d9ec892a24e85815a12c0
Warrior <15819064843@163.com>
2018-09-28 22:32:04 +0800

    Update FindPivotIndex.java

--- 算法模块/FindPivotIndex.java
+++ 算法模块/FindPivotIndex.java
@@ -1,3 +1,10 @@
+/**
+* 寻找排序数组中心索引
+* @author Guozhu Zhu
+* @date 2018/9/28
+* @version 1.0
+*
+*/
 class Solution {
     
     public int pivotIndex(int[] nums) {
@@ -12,7 +19,7 @@ class Solution {
         int rightSum = 0;
         for (int i = 0; i < nums.length; i++) {
             if (i == 0) {
-                leftSum = 0;
+                leftSum = 0; //单独特殊考虑 leftSum = 0;
             } else {
                 leftSum += nums[i-1];
             }
