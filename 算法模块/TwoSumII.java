b3199b507990db92926c48e03e0337539a5f3756
zhuguozhu123 <zgz1211651004>
2018-10-09 19:16:13 +0800

    zhuguozhu

---
+++ 算法模块/TwoSumII.java
@@ -0,0 +1,17 @@
+class Solution {
+    
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i); 
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target-nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        return null;
+    }
+    
+}
