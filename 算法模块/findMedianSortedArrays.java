fc6ad1c7ca44a4bf5976989ac0b3fad6210b6a93
Warrior <15819064843@163.com>
2018-10-09 19:44:42 +0800

    Update findMedianSortedArrays.java

--- 算法模块/findMedianSortedArrays.java
+++ 算法模块/findMedianSortedArrays.java
@@ -1,5 +1,12 @@
+/**
+* 两个排序数组的中位数
+* @author Guozhu Zhu
+* @date 2018/10/09
+* @version 1.0
+*/
 class Solution {
     
+    //采用了归并的思想
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] temp = new int[nums1.length+nums2.length];
         int i = 0;
@@ -20,6 +27,7 @@ class Solution {
         while (j <= n) {
             temp[k++] = nums2[j++];
         }
+        //处理偶数个元素时
         if (k%2 == 0) {
             return (temp[(k/2-1)]+temp[k/2]) / 2.0;
         }
