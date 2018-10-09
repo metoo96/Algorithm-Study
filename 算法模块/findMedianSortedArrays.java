76d5c30551b17879b0f10a1c01b593423358a59d
zhuguozhu123 <zgz1211651004>
2018-10-09 19:42:05 +0800

    zhuguozhu

---
+++ 算法模块/findMedianSortedArrays.java
@@ -0,0 +1,29 @@
+class Solution {
+    
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] temp = new int[nums1.length+nums2.length];
+        int i = 0;
+        int j = 0;
+        int m = nums1.length-1;
+        int n = nums2.length-1;
+        int k = 0;
+        while (i <= m && j <= n) {
+            if (nums1[i] < nums2[j]) {
+                temp[k++] = nums1[i++];
+            } else {
+                temp[k++] = nums2[j++];
+            }
+        }
+        while (i <= m) {
+            temp[k++] = nums1[i++];
+        }
+        while (j <= n) {
+            temp[k++] = nums2[j++];
+        }
+        if (k%2 == 0) {
+            return (temp[(k/2-1)]+temp[k/2]) / 2.0;
+        }
+        return temp[k/2];
+    }
+    
+}
