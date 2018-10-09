39d98de43bcb7fc4a19170dffb46b44168ee6f20
Warrior <15819064843@163.com>
2018-10-09 19:18:33 +0800

    keep going

--- 算法模块/TwoSumII.java
+++ 算法模块/TwoSumII.java
@@ -1,9 +1,15 @@
+/**
+* 两数之和等于目标值
+* @author Guozhu Zhu
+* @date 2018/10/09
+* @version 1.0
+*/
 class Solution {
     
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i); 
+            map.put(nums[i], i);    //K: 为数组的值， V: 数组的下标；
         }
         for (int i = 0; i < nums.length; i++) {
             int complement = target-nums[i];
