af66a4e602b9b8fca4bcf950a9c60aed31069871
zhuguozhu123 <zgz1211651004>
2018-10-10 22:46:53 +0800

    zhuguozhu

---
+++ 算法模块/IsBST.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+/*
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Checker {
+    
+    public boolean checkBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        TreeNode p = root;
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        int temp = Integer.MIN_VALUE;
+        while (p != null || !s.isEmpty()) {
+            while (p != null) {
+                s.push(p);
+                p = p.left;
+            } 
+            if (!s.isEmpty()) {
+                p = s.pop();
+                if (p.val <= temp) {
+                    return false;
+                }
+                temp = p.val;
+                p = p.right;
+            }
+        }
+        return true;
+    }
+    
+}
