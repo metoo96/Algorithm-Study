8f7a7a0e3d9a24bec55b2b00ac179c375b44baa7
zhuguozhu123 <zgz1211651004>
2018-10-25 23:46:49 +0800

    zhuguozhu

---
+++ 算法模块/hasPathSum.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public boolean hasPathSum(TreeNode root, int sum) {
+         if (root == null)
+            return false;
+        return hasPathSumCore(root, 0, sum);
+    }
+     
+    public boolean hasPathSumCore(TreeNode root, int sum, int target) {
+        if (root == null)
+            return false;
+        if (root.left == null && root.right == null && target == sum+root.val)
+            return true;
+        if (root.left == null && root.right == null)
+            return false;
+        return hasPathSumCore(root.left, sum+root.val, target) || hasPathSumCore(root.right, sum+root.val, target);
+    }
+    
+}
