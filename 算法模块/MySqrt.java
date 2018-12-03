fa52603723e4b9a3c994789aa30d9eecfb34ef5b
zhuguozhu123 <zgz1211651004>
2018-12-03 17:24:16 +0800

    zhuguozhu

---
+++ 算法模块/MySqrt.java
@@ -0,0 +1,19 @@
+class Solution {
+    
+    public int mySqrt(int x) {
+        if (x <= 1) {
+            return x;
+        }
+        int left = 0, right = x;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(x / mid >= mid) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return right - 1;
+    }
+    
+}
