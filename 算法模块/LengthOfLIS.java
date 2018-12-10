7c8a7d9e3e1fa3fd4fe8b9366b751a1e7cdcf45c
Warrior <15819064843@163.com>
2018-12-10 21:59:57 +0800

    Update LengthOfLIS.java

--- 算法模块/LengthOfLIS.java
+++ 算法模块/LengthOfLIS.java
@@ -1,24 +1,31 @@
+/**
+
+* 最长上升子序列
+* @author Guozhu Zhu
+* @date 2018/12/10
+* @version 1.0 
+*/
 class Solution {
     
     public int lengthOfLIS(int[] arr) { 
         if (arr == null || arr.length == 0) {
             return 0;
         }
-		int[] dp = new int[arr.length];
-		int lis = 0;
-		for (int i = 0; i < arr.length; i++) {
-			dp[i] = 1;
-			for (int j = 0; j < i; j++) {
-				if (arr[i] > arr[j] && dp[i] < dp[j]+1) {
-					dp[i] = dp[j]+1;
-				}
-            }
+        int[] dp = new int[arr.length];
+	int lis = 0;
+	for (int i = 0; i < arr.length; i++) {
+		dp[i] = 1;
+		for (int j = 0; j < i; j++) {
+		if (arr[i] > arr[j] && dp[i] < dp[j]+1) {
+			dp[i] = dp[j]+1;
 		}
+            }
+	}
         for (int i = 0; i < arr.length; i++) {
             if (dp[i] > lis) 
                 lis = dp[i];
         }
-		return lis;
+	return lis;
     }
     
 }
