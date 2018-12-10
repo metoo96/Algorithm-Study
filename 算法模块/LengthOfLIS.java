157b9d1d7f94cf67b04c2ec23dce72f65ab29b7e
zhuguozhu123 <zgz1211651004>
2018-12-10 21:55:02 +0800

    zhuguozhu

---
+++ 算法模块/LengthOfLIS.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    public int lengthOfLIS(int[] arr) { 
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+		int[] dp = new int[arr.length];
+		int lis = 0;
+		for (int i = 0; i < arr.length; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (arr[i] > arr[j] && dp[i] < dp[j]+1) {
+					dp[i] = dp[j]+1;
+				}
+            }
+		}
+        for (int i = 0; i < arr.length; i++) {
+            if (dp[i] > lis) 
+                lis = dp[i];
+        }
+		return lis;
+    }
+    
+}
