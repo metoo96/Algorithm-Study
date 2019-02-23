3e52e3d23deb2fead19b622eeec9937bf2348cd5
zhuguozhu123 <15819064843@163.com>
2019-02-24 03:21:13 +0800

    zhuguozhu

---
+++ 算法模块/HasSubtree.java
@@ -0,0 +1,50 @@
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+/**
+ * @author  Guozhu Zhu
+ * @date  2019/2/24
+ * @version  1.0
+ */
+public class Solution {
+
+    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        boolean result = false;
+        if (root1 != null && root2 != null) {
+            if (root1.val == root2.val) {
+                result = doesTree1HasTree2(root1, root2);
+            }
+            if (!result) {
+                result = HasSubtree(root1.left, root2);
+            }
+            if (!result) {
+                result = HasSubtree(root1.right, root2);
+            }
+        }
+        return result;
+    }
+    
+    public boolean doesTree1HasTree2(TreeNode root1, TreeNode root2) {
+        if (root2 == null) {
+            return true;
+        }
+        if (root1 == null) {
+            return false;
+        }
+        if (root1.val != root2.val) {
+            return false;
+        }
+        return doesTree1HasTree2(root1.left, root2.left) && doesTree1HasTree2(root1.right, root2.right);
+    }
+    
+}
